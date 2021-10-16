7aa2cf3bd3bccbe5d25bf67305d104c3593ab051
Jensen Zhang <36783874+Jen-Jon@users.noreply.github.com>
2021-10-16 13:37:23 +0800

    Add files via upload

---
+++ srcs/he-simple-common.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://cdn.jsdelivr.net/gh/Jen-Jon/CDN_Bank/srcs/he-simple.css'
+  var s = d.createElement('script')
+  s.src = 'https://widget.heweather.net/simple/static/js/he-simple.js?v=1.4.0'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
