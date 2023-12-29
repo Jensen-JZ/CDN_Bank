2bcb15a45393805335092929d52e64fc2d161157
Jensen J. ZHANG <36783874+Jen-Jon@users.noreply.github.com>
2023-12-29 14:32:17 +0800

    Update and rename he-simple-common-v2.1.js to he-simple-common-v2.0.1.js

--- srcs/qweather/he-simple-common-v2.0.1.js
+++ srcs/qweather/he-simple-common-v2.0.1.js
@@ -1,7 +1,7 @@
 (function (d) {
   var c = d.createElement('link')
   c.rel = 'stylesheet'
-  c.href = 'https://cdn.jsdelivr.net/gh/Jen-Jon/CDN_Bank/srcs/qweather/he-simple-v1.5.css'
+  c.href = 'https://cdn.jsdelivr.net/gh/Jen-Jon/CDN_Bank/srcs/qweather/he-simple-v1.50.css'
   var s = d.createElement('script')
   s.src = 'https://cdn.jsdelivr.net/gh/Jen-Jon/CDN_Bank/srcs/qweather/he-simple-v1.5.js'
   var sn = d.getElementsByTagName('script')[0]
