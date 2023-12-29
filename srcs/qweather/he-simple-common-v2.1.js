a1a5b350adb77fe13761105ef18d1017f6fe2dd1
Jensen J. ZHANG <36783874+Jen-Jon@users.noreply.github.com>
2023-12-29 14:25:45 +0800

    Create he-simple-common-v2.1.js

---
+++ srcs/qweather/he-simple-common-v2.1.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'https://cdn.jsdelivr.net/gh/Jen-Jon/CDN_Bank/srcs/qweather/he-simple-v1.5.css'
+  var s = d.createElement('script')
+  s.src = 'https://cdn.jsdelivr.net/gh/Jen-Jon/CDN_Bank/srcs/qweather/he-simple-v1.5.js'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
